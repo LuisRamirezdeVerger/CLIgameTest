f598fdf1b75f78d9008ac4d7f7e99830c557a0de
Luis RdV <job.wito.uk@gmail.com>
2021-12-26 16:47:55 +0000

    movement added, good logic, not that coding

--- qstns.js
+++ qstns.js
@@ -1,3 +1,5 @@
+const colors = require("colors");
+
 const tutorial = [
   {
     type: "text",
@@ -10,11 +12,36 @@ const prologe = [
   {
     type: "input",
     name: "charName",
-    message: "Hey dude, are you okay? What's your name?",
+    message: "Hey dude, are you okay? What's your name? >>",
   },
 ];
+
+const room00 = [
+  {
+    type: "input",
+    name: "room00",
+    message: "What's your next movement? >>".green,
+  },
+];
+
+const room01 = [
+  {
+    type: "input",
+    name: "room01",
+    message: "You are in north. What's your next movement? >>".purple,
+  },
+];
+
+// const console = [
+//   {
+//     name:conSole
+//     message: `Alright ${game.name},
+//     // I'm gonna get you out of there`.blue,
+//   },
+// ];
 
 module.exports = {
   tutorial,
   prologe,
+  room00,
 };
--- scapeRoom.js
+++ scapeRoom.js
@@ -1,17 +1,54 @@
 const inquirer = require("inquirer");
-const { tutorial, prologe } = require("./qstns");
+const colors = require("colors");
+const { tutorial, prologe, room00 } = require("./qstns");
 const { Char } = require("./class");
 
 let game;
 
 const init = () => {
+  // const style = "font-weight: bold";
+  // console.log("Teeeeeest", style);
   inquirer.prompt(tutorial).then(() => tuto());
 };
 
 const tuto = () => {
-  inquirer.prompt(prologe).then((answers) => {
-    game = new Char(answers.charName);
-    console.log(`Alright, ${game.name}, I'm gonna get you out of there`);
+  inquirer
+    .prompt(prologe)
+    .then((answers) => {
+      game = new Char(answers.charName);
+      console.log(
+        `Alright ${game.name},
+I'm gonna get you out of there`.blue
+      );
+    })
+    .then(() => room0());
+};
+
+const room0 = () => {
+  console.log("Looks like you're in the main door");
+  // inquirer.prompt(room00);
+  inquirer.prompt(room00).then((answer) => {
+    if (answer.room00 === "go north") {
+      console.log("You move");
+      north();
+    } else {
+      console.log("Command invalid, think a bit harder!");
+      room0();
+    }
   });
 };
+
+const north = () => {
+  console.log("You went to north");
+  // inquirer.prompt(room00).then((answer) => {
+  //   if (answer.room00 === "move") {
+  //     console.log("You move");
+  //     north();
+  //   } else {
+  //     console.log("error");
+  //     room0();
+  //   }
+  // });
+};
+
 init();
