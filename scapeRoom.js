fba362ed1c0dcf65fea5b85427aae4b3e926dcc1
Luis RdV <job.wito.uk@gmail.com>
2021-12-27 22:30:43 +0000

    Help added and 1st room finished!

--- scapeRoom.js
+++ scapeRoom.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const colors = require("colors");
-const { tutorial, prologe, room00 } = require("./qstns");
+const qstns = require("./qstns");
 const { Char } = require("./class");
 
 let game;
@@ -8,12 +8,12 @@ let game;
 const init = () => {
   // const style = "font-weight: bold";
   // console.log("Teeeeeest", style);
-  inquirer.prompt(tutorial).then(() => tuto());
+  inquirer.prompt(qstns.tutorial).then(() => tuto());
 };
 
 const tuto = () => {
   inquirer
-    .prompt(prologe)
+    .prompt(qstns.prologe)
     .then((answers) => {
       game = new Char(answers.charName);
       console.log(
@@ -27,28 +27,31 @@ I'm gonna get you out of there`.blue
 const room0 = () => {
   console.log("Looks like you're in the main door");
   // inquirer.prompt(room00);
-  inquirer.prompt(room00).then((answer) => {
-    if (answer.room00 === "go north") {
-      console.log("You move");
+  inquirer.prompt(qstns.room00).then((answer) => {
+    if (answer.room00 === "help") {
+      console.log(
+        "You need to scape, use actions (commands) that you think you could do in you were there"
+      );
+      room0();
+    } else if (answer.room00 === "open door") {
+      console.log("Good thinking!");
       north();
+    } else if (answer.room00 === "look around") {
+      console.log(
+        "There's not much to see, you're in a 2x2 room with 3 windows and a door"
+      );
+      room0();
+    } else if (answer.room00 === "jump") {
+      console.log("You jumped! What's a game without jumping, right?");
+      room0();
     } else {
       console.log("Command invalid, think a bit harder!");
       room0();
     }
   });
-};
 
-const north = () => {
-  console.log("You went to north");
-  // inquirer.prompt(room00).then((answer) => {
-  //   if (answer.room00 === "move") {
-  //     console.log("You move");
-  //     north();
-  //   } else {
-  //     console.log("error");
-  //     room0();
-  //   }
-  // });
+  const north = () => {
+    console.log("You went to north");
+  };
 };
-
 init();
